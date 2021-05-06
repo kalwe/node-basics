81d362ab7a818cab651b13cc8f196588fd4a0f1a
KalweCaramalac <kalwe.dev@gmail.com>
2021-05-06 03:33:30 -0400

    some built-in modules

---
+++ builtin/path.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+console.log(path.sep)
+
+const filePath = path.join('/content/', 'sub', 'file.txt')
+console.log(filePath)
+
+const base = path.basename(filePath)
+console.log(base)
+
+const absolute = path.resolve(__dirname, 'content', 'sub', 'file.txt')
+console.log(absolute)
